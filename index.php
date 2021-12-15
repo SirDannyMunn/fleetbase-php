3e04a60ebcdde96377faabb9310b90359c6055f9
TakerunYamazaki <mdniamatullah.khan@gmail.com>
2021-12-15 15:14:46 +0600

    key and curl code removded from index file

--- index.php
+++ index.php
@@ -28,31 +28,6 @@
 	});*/
 	//contact.save();
 	</script>
-
-
-	<?php
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://api.fleetbase.io/v1/contacts',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    '-u: flb_live_L2HVcQF9SzH5hJFWI9Hx'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
-?>
 </head>
 <body>
 
