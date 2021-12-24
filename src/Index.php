74e1fda6e04c43c561e2f0d53994816d4cfd0829
amit panchal <95898632+panchalamit@users.noreply.github.com>
2021-12-24 16:07:22 +0530

    Add files via upload

---
+++ src/Index.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace Index;
+
+class Index {
+     /**
+     * get IP address of the user
+     *
+     * @return void
+     */
+    public function list()
+    {
+        echo "This is the fleetbase PHP API";
+    }
+}
+
+
+?>
